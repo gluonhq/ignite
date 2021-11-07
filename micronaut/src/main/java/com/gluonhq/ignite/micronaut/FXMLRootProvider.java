115be2ca9c972b79667dc56bef7e7a2ad602a76c
Vinicius Carvalho <vcarvalho@netflix.com>
2021-11-07 17:33:21 -0500

    Updating to micronaut 2.5.13

--- micronaut/src/main/java/com/gluonhq/ignite/micronaut/FXMLRootProvider.java
+++ micronaut/src/main/java/com/gluonhq/ignite/micronaut/FXMLRootProvider.java
@@ -72,11 +72,11 @@ public class FXMLRootProvider {
         return node;
     }
 
-    private String getViewPath(@NotBlank Class<?> cls) {
+    private String getViewPath(Class<?> cls) {
         return cls.getName().replace('.', '/');
     }
 
-    private String withExt( @NotBlank String viewName, @NotBlank String extName ) {
+    private String withExt(String viewName, String extName ) {
         String ext = extName.startsWith(".")? extName: "." + extName;
         return String.format("/%s%s", viewName, ext);
     }
